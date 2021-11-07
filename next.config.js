2272daa54431c3c29a6c143c084fae73c5beefce
ruslan.shukiurov <sukurovruslan68@gmail.com>
2021-11-07 13:25:13 +0200

    include next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-// const withPWA = require('next-pwa')
-// const runtimeCaching = require('next-pwa/cache')
-//
-// module.exports = withPWA({
-//   pwa: {
-//     dest: 'public',
-//     runtimeCaching,
-//   },
-// })
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+})
