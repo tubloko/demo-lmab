80893d1d4d2a083bb77f3b65f6013d5ccd02246d
ruslan.shukiurov <sukurovruslan68@gmail.com>
2021-11-07 14:06:22 +0200

    delete redirect in getInitialProps

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
+    // dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
     runtimeCaching,
   },
 })
